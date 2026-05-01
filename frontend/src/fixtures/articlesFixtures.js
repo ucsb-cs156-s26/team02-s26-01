71fd7a09e6c6a2bd271db1075e7cd45f8a06d1f2
schen <124324067+Junyixi@users.noreply.github.com>
2026-04-30 18:21:20 -0700

    Add article fixtures for testing

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,1 +1,30 @@
+const articlesFixtures = {
+  oneArticle: {
+    id: 1,
+    quarterYYYYQ: "20221",
+    name: "Noon on January 2nd",
+    localDateTime: "2022-01-02T12:00:00",
+  },
+  threeArticles: [
+    {
+      id: 1,
+      quarterYYYYQ: "20221",
+      name: "Noon on January 2nd",
+      localDateTime: "2022-01-02T12:00:00",
+    },
+    {
+      id: 2,
+      quarterYYYYQ: "20222",
+      name: "Noon on April 3rd",
+      localDateTime: "2022-04-03T12:00:00",
+    },
+    {
+      id: 3,
+      quarterYYYYQ: "20223",
+      name: "Noon on July 4th",
+      localDateTime: "2022-07-04T12:00:00",
+    },
+  ],
+};
 
+export { articlesFixtures };
