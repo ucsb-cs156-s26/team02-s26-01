7c2c9ca6bf9ca226bec3e5861c1d5b3a539448f1
schen <124324067+Junyixi@users.noreply.github.com>
2026-05-01 11:23:50 -0700

    Update articlesFixtures with new article details

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,30 +1,49 @@
 const articlesFixtures = {
   oneArticle: {
     id: 1,
-    quarterYYYYQ: "20221",
-    name: "Noon on January 2nd",
+    title: "This Is the Life",
+    url: "https://www.billemory.com/dillard/dillard.html",
+    explanation: "You enjoy work and will love your grandchildren, and somewhere in there you die",
+    email: "dillarda@gmail.com",
     localDateTime: "2022-01-02T12:00:00",
   },
   threeArticles: [
     {
       id: 1,
-      quarterYYYYQ: "20221",
-      name: "Noon on January 2nd",
+      title: "What Is Math?",
+      url: "https://www.smithsonianmag.com/science-nature/what-math-180975882/",
+      explanation: "A teenager asked that age-old question on TikTok, creating a viral backlash, and then, a thoughtful scientific debate...",
+      email: "falkd@gmail.com",
       localDateTime: "2022-01-02T12:00:00",
     },
     {
       id: 2,
-      quarterYYYYQ: "20222",
-      name: "Noon on April 3rd",
+      title: "Write Like a Mofo",
+      url: "https://therumpus.net/2010/08/19/dear-sugar-the-rumpus-advice-column-48-write-like-a-motherfucker/",
+      explanation: "Raw, emotional adivce on the role of humility and surrender in the often tortured world of the writer",
+      email: "strayedc@gmail.com",
       localDateTime: "2022-04-03T12:00:00",
     },
     {
       id: 3,
-      quarterYYYYQ: "20223",
-      name: "Noon on July 4th",
+      title: "You Can't Kill the Rooster",
+      url: "https://sites.google.com/site/mendomundo/home/you-cant-kill-the-rooster",
+      explanation: "Use the word y'all and, before you knew it, you'd find yourself in a haystack French-kissing an underage goat",
+      email: "sedarisd@gmail.com",
       localDateTime: "2022-07-04T12:00:00",
     },
   ],
 };
 
 export { articlesFixtures };
+  private String title;
+  private String url;
+  private String explanation;
+  private String email;
+  private LocalDateTime dateAdded;
+.param("title", "Sample Title")
+                .param("url", "http://example.com")
+                .param("explanation", "Sample explanation")
+                .param("email", "test@example.com")
+                .param("dateAdded", "2022-01-03T00:00:00")
+
