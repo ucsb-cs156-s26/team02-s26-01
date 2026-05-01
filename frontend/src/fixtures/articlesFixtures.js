937a1ad26d0903b9fced9f5ebd3056ac5f8b8ff2
schen <124324067+Junyixi@users.noreply.github.com>
2026-05-01 11:35:15 -0700

    Rename articleFixtures to articlesFixtures

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,9 +1,10 @@
-const articleFixtures = {
+const articlesFixtures = {
   oneArticle: {
     id: 1,
     title: "This Is the Life",
     url: "https://www.billemory.com/dillard/dillard.html",
-    explanation: "You enjoy work and will love your grandchildren, and somewhere in there you die",
+    explanation:
+      "You enjoy work and will love your grandchildren, and somewhere in there you die",
     email: "dillarda@gmail.com",
     localDateTime: "2022-01-02T12:00:00",
   },
@@ -12,7 +13,8 @@ const articleFixtures = {
       id: 1,
       title: "What Is Math?",
       url: "https://www.smithsonianmag.com/science-nature/what-math-180975882/",
-      explanation: "A teenager asked that age-old question on TikTok, creating a viral backlash, and then, a thoughtful scientific debate...",
+      explanation:
+        "A teenager asked that age-old question on TikTok, creating a viral backlash, and then, a thoughtful scientific debate...",
       email: "falkd@gmail.com",
       localDateTime: "2022-01-02T12:00:00",
     },
@@ -20,7 +22,8 @@ const articleFixtures = {
       id: 2,
       title: "Write Like a Mofo",
       url: "https://therumpus.net/2010/08/19/dear-sugar-the-rumpus-advice-column-48-write-like-a-motherfucker/",
-      explanation: "Raw, emotional adivce on the role of humility and surrender in the often tortured world of the writer",
+      explanation:
+        "Raw, emotional adivce on the role of humility and surrender in the often tortured world of the writer",
       email: "strayedc@gmail.com",
       localDateTime: "2022-04-03T12:00:00",
     },
@@ -28,12 +31,13 @@ const articleFixtures = {
       id: 3,
       title: "You Can't Kill the Rooster",
       url: "https://sites.google.com/site/mendomundo/home/you-cant-kill-the-rooster",
-      explanation: "Use the word y'all and, before you knew it, you'd find yourself in a haystack French-kissing an underage goat",
+      explanation:
+        "Use the word y'all and, before you knew it, you'd find yourself in a haystack French-kissing an underage goat",
       email: "sedarisd@gmail.com",
       localDateTime: "2022-07-04T12:00:00",
     },
   ],
 };
 
-export { articleFixtures };
+export { articlesFixtures };
 
