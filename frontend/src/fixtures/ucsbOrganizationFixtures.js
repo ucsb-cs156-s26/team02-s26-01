81149e76efa4a05d5a288ec0e06951b187fc2e61
Andy Wang <yue636@ucsb.edu>
2026-05-05 11:40:18 -0700

    added UCSBOrg fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,33 @@
+const ucsbOrganizationFixtures = {
+  oneOrganization: [
+    {
+      orgCode: "DSClub",
+      orgTranslationShort: "DSC",
+      orgTranslation: "Data Science Club",
+      inactive: false,
+    },
+  ],
+
+  threeOrganizations: [
+    {
+      orgCode: "DSClub",
+      orgTranslationShort: "DSC",
+      orgTranslation: "Data Science Club",
+      inactive: false,
+    },
+    {
+      orgCode: "ACM",
+      orgTranslationShort: "ACM",
+      orgTranslation: "Association for Computing Machinery",
+      inactive: false,
+    },
+    {
+      orgCode: "Chess",
+      orgTranslationShort: "Chess",
+      orgTranslation: "Chess Club",
+      inactive: true,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
