061041ec16077c85f1ca677e54c0a1ba4a646e2c
schen <124324067+Junyixi@users.noreply.github.com>
2026-05-04 17:33:57 -0700

    Refactor ArticlesTable component for consistency

--- frontend/src/main/components/Articles/ArticlesTable.jsx
+++ frontend/src/main/components/Articles/ArticlesTable.jsx
@@ -5,15 +5,11 @@ import { useBackendMutation } from "main/utils/useBackend";
 import {
   cellToAxiosParamsDelete,
   onDeleteSuccess,
-} from "main/utils/articleUtils";
+} from "main/utils/articlesUtils";
 import { useNavigate } from "react-router";
 import { hasRole } from "main/utils/useCurrentUser";
 
-export default function ArticlesTable({
-  articles,
-  currentUser,
-  testIdPrefix = "ArticlesTable",
-}) {
+export default function ArticlesTable({ articles, currentUser }) {
   const navigate = useNavigate();
 
   const editCallback = (cell) => {
@@ -40,35 +36,37 @@ export default function ArticlesTable({
       accessorKey: "id", // accessor is the "key" in the data
     },
     {
-      header: "Title",
+      header: "title",
       accessorKey: "title",
     },
     {
-      header: "URL",
+      header: "url",
       accessorKey: "url",
     },
     {
-      header: "Explanation",
+      header: "explanation",
       accessorKey: "explanation",
     },
     {
-      header: "Email",
+      header: "email",
       accessorKey: "email",
     },
     {
-      header: "Date Added",
+      header: "dateAdded",
       accessorKey: "dateAdded",
     },
   ];
 
   if (hasRole(currentUser, "ROLE_ADMIN")) {
-    columns.push(ButtonColumn("Edit", "primary", editCallback, testIdPrefix));
     columns.push(
-      ButtonColumn("Delete", "danger", deleteCallback, testIdPrefix),
+      ButtonColumn("Edit", "primary", editCallback, "ArticlesTable"),
+    );
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, "ArticlesTable"),
     );
   }
 
   return (
-    <OurTable data={articles} columns={columns} testid={testIdPrefix} />
+    <OurTable data={articles} columns={columns} testid={"ArticlesTable"} />
   );
 }
