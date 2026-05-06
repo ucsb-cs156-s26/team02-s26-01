3c31643b07543a21f02c529cde950b0cceda3eea
Oscar Valeriano <oscarvaleriano@ucsb.edu>
2026-05-05 17:28:01 -0700

    Menu Item Review create fixtures - fixing formatting

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -2,32 +2,37 @@ import { ThreeItemsAdminUser } from "stories/components/Restaurants/RestaurantTa
 
 const menuItemReviewFixtures = {
   oneMenuItemReview: {
-    
+    id: 1,
+    itemId: 67,
+    reviewerEmail: "oscarvaleriano@ucsb.edu",
+    stars: 5,
+    dateReviewed: "2026-05-05T02:18:52",
+    comments: "no comments",
   },
   threeMenuItemReviews: [
     {
-      "id": 1,
-      "itemId": 67,
-      "reviewerEmail": "oscarvaleriano@ucsb.edu",
-      "stars": 5,
-      "dateReviewed": "2026-05-05T02:18:52",
-      "comments": "no comments"
+      id: 1,
+      itemId: 67,
+      reviewerEmail: "oscarvaleriano@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2026-05-05T02:18:52",
+      comments: "no comments",
     },
     {
-      "id": 2,
-      "itemId": 23,
-      "reviewerEmail": "oscarvaleriano@ucsb.edu",
-      "stars": 5,
-      "dateReviewed": "2026-05-05T05:13:50",
-      "comments": "Testing"
+      id: 2,
+      itemId: 23,
+      reviewerEmail: "oscarvaleriano@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2026-05-05T05:13:50",
+      comments: "Testing",
     },
     {
-      "id": 3,
-      "itemId": 23,
-      "reviewerEmail": "oscarvaleriano@ucsb.edu",
-      "stars": 5,
-      "dateReviewed": "2026-05-05T05:13:50",
-      "comments": "Testing"
+      id: 3,
+      itemId: 23,
+      reviewerEmail: "oscarvaleriano@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2026-05-05T05:13:50",
+      comments: "Testing",
     },
   ],
 };
