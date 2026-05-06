7cb4a40b07627f24670a8c4c4a069c7cbc8ba0d5
Oscar Valeriano <oscarvaleriano@ucsb.edu>
2026-05-05 19:01:58 -0700

    finished MenuItemReviewForm

--- frontend/src/main/components/MenuItemReview/MenuItemReviewForm.jsx
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.jsx
@@ -16,7 +16,7 @@ function MenuItemReviewForm({
   // Stryker restore all
 
   const navigate = useNavigate();
-
+  const isodate_regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
   const testIdPrefix = "MenuItemReviewForm";
 
   return (
@@ -68,6 +68,63 @@ function MenuItemReviewForm({
         />
         <Form.Control.Feedback type="invalid">
           {errors.reviewerEmail?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="stars">Stars</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-stars"}
+          id="stars"
+          type="number"
+          isInvalid={Boolean(errors.stars)}
+          {...register("stars", {
+            required: "Stars is required.",
+            min: {
+              value: 1,
+              message: "Stars must be at least 1.",
+            },
+            max: {
+              value: 5,
+              message: "Stars must be at most 5.",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.stars?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      
+      <Form.Group className="mb-3">
+            <Form.Label htmlFor="dateReviewed">Date Reviewed (iso format)</Form.Label>
+            <Form.Control
+              data-testid={testIdPrefix + "-dateReviewed"}
+              id="dateReviewed"
+              type="datetime-local"
+              isInvalid={Boolean(errors.dateReviewed)}
+              {...register("dateReviewed", {
+                required: true,
+                pattern: isodate_regex,
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.dateReviewed && "Date Reviewed is required. "}
+            </Form.Control.Feedback>
+          </Form.Group>
+      
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="comments">Comments</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-comments"}
+          id="comments"
+          type="text"
+          isInvalid={Boolean(errors.comments)}
+          {...register("comments", {
+            required: "Comments is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.comments?.message}
         </Form.Control.Feedback>
       </Form.Group>
 
