a7b5684c15e44389f4c6b5748036e2b4ccf71823
Andy Wang <yue636@ucsb.edu>
2026-05-06 17:27:22 -0700

    Created the create page

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router";
@@ -12,6 +12,17 @@ import { expect } from "vitest";
 describe("UCSBOrganizationCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
+  const setupAdminUser = () => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.adminUser);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  };
+
   const setupUserOnly = () => {
     axiosMock.reset();
     axiosMock.resetHistory();
@@ -23,26 +34,88 @@ describe("UCSBOrganizationCreatePage tests", () => {
       .reply(200, systemInfoFixtures.showingNeither);
   };
 
-  const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
+  const getQueryClient = () =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
 
+  test("Renders expected content", async () => {
     setupUserOnly();
 
-    // act
     render(
-      <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={getQueryClient()}>
         <MemoryRouter>
           <UCSBOrganizationCreatePage />
         </MemoryRouter>
       </QueryClientProvider>,
     );
 
-    // assert
+    await screen.findByText("Create New UCSBOrganization");
 
-    await screen.findByText("Create page not yet implemented");
-    expect(
-      screen.getByText("Create page not yet implemented"),
-    ).toBeInTheDocument();
+    expect(screen.getByText("Create New UCSBOrganization")).toBeInTheDocument();
+    expect(screen.getByText("Org Code")).toBeInTheDocument();
+    expect(screen.getByText("Org Translation Short")).toBeInTheDocument();
+    expect(screen.getByText(/^Org Translation$/)).toBeInTheDocument();
+    expect(screen.getByText("Inactive")).toBeInTheDocument();
+    expect(screen.getByText("Create")).toBeInTheDocument();
+  });
+
+  test("on submit, makes request to backend", async () => {
+    setupAdminUser();
+
+    axiosMock.onPost("/api/UCSBOrganization/post").reply(200, {
+      orgCode: "DSClub",
+      orgTranslationShort: "DSC",
+      orgTranslation: "Data Science Club",
+      inactive: "false",
+    });
+
+    render(
+      <QueryClientProvider client={getQueryClient()}>
+        <MemoryRouter>
+          <UCSBOrganizationCreatePage storybook={true} />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await screen.findByText("Create New UCSBOrganization");
+
+    const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
+    const orgTranslationShortField = screen.getByTestId(
+      "UCSBOrganizationForm-orgTranslationShort",
+    );
+    const orgTranslationField = screen.getByTestId(
+      "UCSBOrganizationForm-orgTranslation",
+    );
+    const inactiveField = screen.getByTestId("UCSBOrganizationForm-inactive");
+    const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
+
+    fireEvent.change(orgCodeField, {
+      target: { value: "DSClub" },
+    });
+    fireEvent.change(orgTranslationShortField, {
+      target: { value: "DSC" },
+    });
+    fireEvent.change(orgTranslationField, {
+      target: { value: "Data Science Club" },
+    });
+    fireEvent.change(inactiveField, {
+      target: { value: "false" },
+    });
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      orgCode: "DSClub",
+      orgTranslationShort: "DSC",
+      orgTranslation: "Data Science Club",
+      inactive: "false",
+    });
   });
 });
